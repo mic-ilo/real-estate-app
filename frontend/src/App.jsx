39062b2f9a76634395d1ba89087ed1388a8ff962
mic-ilo <michelaquino1003@gmail.com>
2023-11-14 19:54:18 +0800

    update the ehader and make the profile page private

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import SignUpController from "./controller/SignUpController";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -20,7 +21,9 @@ function App() {
 
         <Route path="/about" element={<About />} />
 
-        <Route path="/profile" element={<Profile />} />
+        <Route element= {<PrivateRoute/>}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
