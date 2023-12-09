6708f983e267e21b6408c870fde22290cc99430f
mic-ilo <michelaquino1003@gmail.com>
2023-12-09 20:55:20 +0800

    add delete user func

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,10 +2,12 @@ import express from "express";
 import { test } from "../controllers/user.controller.js";
 import { updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
+import { deleteUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router;
